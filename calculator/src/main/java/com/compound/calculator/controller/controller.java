a59230fbc04a2aaf1689344914a3fcd70d8f5506
zangetsu45 <mayankgobind02875@gmail.com>
2022-08-11 11:50:04 +0530

    done

--- calculator/src/main/java/com/compound/calculator/controller/controller.java
+++ calculator/src/main/java/com/compound/calculator/controller/controller.java
@@ -2,17 +2,19 @@ package com.compound.calculator.controller;
 
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.compound.calculator.service.service;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/calculator")
 public class controller {
-@GetMapping(path="/calculate",produces=MediaType.APPLICATION_JSON_VALUE)
+@PostMapping(path="/calculate",produces=MediaType.APPLICATION_JSON_VALUE)
 public float answer(@RequestBody service s) {
 	return s.calculate();
 }
